dc0886f8ce433d8df3d27a30b9c88d3ef648c10c
Jesuraj77 <isysraju2001@gmail.com>
2020-03-23 17:13:33 +0530

    Code Addded

---
+++ Level2/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Level2.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult ViewResult()
+        {
+            //implement the code here
+            return View();
+        }
+    }
+}
